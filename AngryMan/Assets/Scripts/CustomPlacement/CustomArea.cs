6ed6e20daca9af5c6fe092b8c45ab8ffdf1dc7bf
Ludvig Grönborg <ludviggronborg@hotmail.com>
2017-02-18 16:32:36 +0100

    A dank start to a dank custom system
    :eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant::eggplant:

---
+++ AngryMan/Assets/Scripts/CustomPlacement/CustomArea.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomArea : ScriptableObject {
+    [Header("Assign squares per lane")]
+    public List<GameObject> m_middleLane;
+    public List<GameObject> m_LeftLane;
+    public List<GameObject> m_rightLane;
+
+    public void InstantiateArea()
+    {
+        GameObject ResultingPrefab = new GameObject();
+        if ((m_middleLane.Count == m_rightLane.Count) && (m_middleLane.Count == m_LeftLane.Count))
+        {
+            // Once per lane
+            for (int i = 0; i < 3; i++)
+            {
+                // Once per square
+                for (int j = 0; j < m_middleLane.Count; j++)
+                {
+
+                }
+            }
+        }
+        else
+            Debug.Log("Lanes are not completed");
+    }
+    private void InstantiateSquare()
+    {
+
+    }
+}
